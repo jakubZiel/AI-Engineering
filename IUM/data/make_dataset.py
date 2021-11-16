00514fd2893e1e180c764bff8e226ba5189005b3
Jakub  Zieliński <jzielins@mion.elka.pw.edu.pl>
2021-11-16 22:14:35 +0100

    processed data

--- IUM/data/make_dataset.py
+++ IUM/data/make_dataset.py
@@ -1,20 +1,68 @@
+from typing import List, Dict
 import json
 
+base_path = "/media/jzielins/SD/sem6/IUM/IUM/data"
 
-def load_json_data(file_path):
-    file = open(file_path, "r")
+def load_json_data(data_name : str) -> Dict :
+    file = open(base_path + "/raw/" +  data_name + "s.jsonl", "r")
     lines = file.readlines()
 
-    objects = []
+    objects = {}
+    id = data_name + "_id"
 
     for line in lines:
-        objects.append(json.loads(line))
+        object_v = json.loads(line)
+        objects[object_v[id]] = object_v
+    
+
+    objects["meta_name"] = data_name
 
     return objects
 
-base_path = "/media/jzielins/SD/sem6/IUM/IUM/data/raw"
 
-users = load_json_data(base_path +  "/users.jsonl", "r")
-sessions = load_json_data(base_path + "/sessions.jsonl", "r")
-products = load_json_data(base_path + "/products.jsonl", "r")
-deliveries = load_json_data(base_path + "/deliveries.jsonl", "r")
+def load_sessions(data_name):
+    file = open(base_path + "/raw/" +  data_name + "s.jsonl", "r")
+    lines = file.readlines()
+
+    sessions = {"meta_name" : "sessions"}
+
+    for line in lines:
+        session : Dict[str, Dict] = json.loads(line)
+        
+        if session["event_type"] == "BUY_PRODUCT":
+            sessions[session["purchase_id"]] = session
+    return sessions
+
+
+def aggregate_data(data_sets : List[Dict[int, Dict]], sessions : Dict):
+    for data_set in data_sets:
+        for key in sessions:
+            if key != "meta_name":
+                session : Dict = sessions[key]
+                
+                id_key = data_set["meta_name"] + "_id"
+                data = data_set[session[id_key]].copy()            
+                del data[id_key]
+
+                del session[id_key]
+                session.update(data)
+
+
+def save_aggregated_data(data : Dict):
+    
+    file = open(base_path + "/processed/sessions.jsonl", "w")
+    for key, value in data.items():
+        if key != "meta_name":
+            json.dump(value, file)
+            file.write("\n")
+
+
+if __name__ == "__main__":
+    users = load_json_data("user")
+    products = load_json_data("product")
+    purchases = load_json_data("purchase")
+    sessions = load_sessions("session")
+    
+    aggregate_data([products, users, purchases], sessions)
+    save_aggregated_data(sessions)
+
