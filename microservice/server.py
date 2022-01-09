55002e2b93e9992d70b0e5da356187b5d0402728
Jakub  Zieliński <jzielins@mion.elka.pw.edu.pl>
2022-01-09 13:11:33 +0100

    persistance

--- microservice/server.py
+++ microservice/server.py
@@ -1,4 +1,25 @@
 from fastapi import FastAPI
-
+from starlette.responses import JSONResponse
+from pydantic import BaseModel
 app = FastAPI()
 
+class Purchase(BaseModel):
+    company : int
+    user_id : int
+    city : int
+
+@app.get("/ab_test")
+async def ab_test() -> JSONResponse:
+    
+    return {
+        "a_accuracy" : 0.45,
+        "b_accuracy" : 0.55
+    }
+
+
+@app.post("/predict")
+async def predict(purchase : Purchase) -> JSONResponse:
+
+    return {
+        "prediction" : 0
+    }
