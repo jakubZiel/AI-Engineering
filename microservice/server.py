5fa5309b6d9807e49be90305a524998012ec6810
Jakub  Zieliński <jzielins@mion.elka.pw.edu.pl>
2022-01-09 15:28:27 +0100

    microserveice serving predictions

--- microservice/server.py
+++ microservice/server.py
@@ -1,9 +1,11 @@
-from fastapi import FastAPI
-from starlette.responses import JSONResponse
+from fastapi import FastAPI, HTTPException
+from pydantic.main import BaseModel
+from starlette.responses import HTMLResponse, JSONResponse
 from Purchase import Purchase
 from joblib import load
 import os
 import uvicorn
+from sklearn import base
 
 models_dir = "models/"
 
@@ -26,15 +28,19 @@ if __name__ == "__main__" :
             "b_accuracy" : 0.55
         }
 
-    @app.post("/predict")
-    async def predict(purchase : Purchase) -> JSONResponse:
-
+    @app.post("/predict/{model_id}")
+    async def predict(model_id : int, purchase : Purchase) -> JSONResponse:        
+        
+        if model_id not in models:
+            raise HTTPException(status_code=404, detail="Model id={model_id} doesn't exist")
+        if purchase is None:
+            raise HTTPException(status_code=400, detail="Request can't be empty")   
         return {
-            "prediction" : 0
+            "prediction" : models[model_id].predict([purchase.toVector()])[0]
         }
 
     @app.get("/models")
-    async def all_models():
+    async def all_models() -> JSONResponse:
         return model_names
 
     uvicorn.run(app)
