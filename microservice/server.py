3cf42bb23ef1464b6b7f86ec541e85dbeaeb4b1b
Jakub  Zieliński <jzielins@mion.elka.pw.edu.pl>
2022-01-09 14:27:37 +0100

    microservice

--- microservice/server.py
+++ microservice/server.py
@@ -1,25 +1,40 @@
 from fastapi import FastAPI
 from starlette.responses import JSONResponse
-from pydantic import BaseModel
+from Purchase import Purchase
+from joblib import load
+import os
+import uvicorn
+
+models_dir = "models/"
+
 app = FastAPI()
 
-class Purchase(BaseModel):
-    company : int
-    user_id : int
-    city : int
+if __name__ == "__main__" :
+    model_files = os.listdir("models")
+    models = {}
+    model_names = {}
 
-@app.get("/ab_test")
-async def ab_test() -> JSONResponse:
-    
-    return {
-        "a_accuracy" : 0.45,
-        "b_accuracy" : 0.55
-    }
+    for index, file in  enumerate(model_files):
+        models[index] = load(models_dir + file)
+        model_names[index] = file.split('.')[0] 
+        
+    @app.get("/ab_test")
+    async def ab_test() -> JSONResponse:
+        
+        return {
+            "a_accuracy" : 0.45,
+            "b_accuracy" : 0.55
+        }
 
+    @app.post("/predict")
+    async def predict(purchase : Purchase) -> JSONResponse:
 
-@app.post("/predict")
-async def predict(purchase : Purchase) -> JSONResponse:
+        return {
+            "prediction" : 0
+        }
 
-    return {
-        "prediction" : 0
-    }
+    @app.get("/models")
+    async def all_models():
+        return model_names
+
+    uvicorn.run(app)
